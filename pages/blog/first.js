5a33db0bf89860a3880a9459256839817e0ab01e
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-04-15 15:22:26 +0530

    setup nextjs

---
+++ pages/blog/first.js
@@ -0,0 +1,4 @@
+function FirstBlog() {
+  return <h1> Second Blog page</h1>;
+}
+export default FirstBlog;
---
+++ pages/blog/second.js
@@ -0,0 +1,4 @@
+function SecondBlog() {
+  return <h1> Second Blog page</h1>;
+}
+export default SecondBlog;
