bcc1457ea94d55ef7c3ff12500eaab1c53c296ab
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-04-18 15:48:13 +0530

    finish with Catch All Routes

---
+++ pages/docs/[[...params]].js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router';
+
+function Doc() {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  console.log(params);
+  if (params.length === 2) {
+    return (
+      <h1>
+        Viewing Docs for Features {params[0]} and concept {params[1]}
+      </h1>
+    );
+  } else if (params.length === 1) {
+    return <h1> Viewing Docs For Features{params[0]}</h1>;
+  }
+  return <h1>Docs Home Pages</h1>;
+}
+
+export default Doc;
