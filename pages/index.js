1c7182a0e5275a079c375472aac07c5d0623b4a2
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-04-19 22:02:30 +0530

    Complete Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('placing your Order');
+    router.replace('/product');
+  };
   return (
     <div>
       <h1> Home Page</h1>
@@ -10,6 +16,7 @@ function Home() {
       <Link href="/Product">
         <h2>Product</h2>
       </Link>
+      <button onClick={handleClick}>PlaceOrder</button>
     </div>
   );
 }
